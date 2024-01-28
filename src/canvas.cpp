378b734d3910dfa7bd135255c0c7f0618ee6d24a
bitkiwi <varkiwibusiness@gmail.com>
2024-01-28 11:49:16 -0800

    worked on windows and tasks

--- src/canvas.cpp
+++ src/canvas.cpp
@@ -14,12 +14,14 @@
 // CANVAS
 ////////////////////
 
-class new_canvas {
+class Canvas{
 public:
 	std::vector<std::vector<char>> matrix;                                     // make a matrix to store the canvas that will be rendered
 	int w;                                                                     // width
 	int h;                                                                     // height
-
+	Canvas(){
+		this->reset();
+	}
 	void reset(){                                                              // function to reset the canvas and update it's size
 		matrix.clear();
 		struct winsize size;                                                   // window size
@@ -34,7 +36,6 @@ public:
 			};
 		};
 	};
-
 	void render(){                                                             // draw the canvas function
 		system("clear");                                                       // clear the screen
 		for(int y = 0; y < w; y++){                                            // iterate
@@ -49,43 +50,63 @@ public:
 	};
 };
 
-new_canvas canvas;                                                             // create the canvas
+Canvas canvas;                                                                 // create the canvas
+
+///////////////////
+// WINDOW MANAGER
+///////////////////
+
+/*
+class WindowManager{
+public:
+	std::vector<Window> windows;
+	Window (*hovered);
+	Window (*focused);
+	WindowManager(){};
+	void newWindow(void (*task_ptr)(int)){
+		windows.push_back(Window(0,));
+	}
+	void hoverWindow(){}
+	void closeWindow(){}
+	void focusWindow(){}
+};
+*/
 
 ///////////////////
 // WINDOW CLASS
 ///////////////////
 
-class window{                                                                  // windows are like mini-canvases
-	public: 
-		int x;                                                                 // positions and size
-		int y;
-		int w;
-		int h;
-		std::function<void(int)> task;                                   // variable holds basic upkeep functions that are ran every cycle
-		window(int pos_x, int pos_y, int size_w, int size_h, void (*task_ptr)(int)){      // constructor
-			x = pos_x;                                                         // pos and size
-			y = pos_y;
-			w = size_w;
-			h = size_h;
-			task = task_ptr;
-		}
-		void handle(int key){
-			for(int draw_y = y; draw_y < y + (h + 1); draw_y += h){            // iterate from top (y) to bottom (y + h)
-				for(int draw_x = x; draw_x < x + w; draw_x++){                 // iterate to draw a line from (x) to (x + w)
-					canvas.draw(draw_x,draw_y,'-');                            // draw at x, y
-				}
+class Window{                                                                  // windows are like mini-canvases
+public:
+	int x;                                                                     // positions and size
+	int y;
+	int w;
+	int h;
+	void (*task)(Window&,int);                                                 // variable holds the task to be ran in window ie. editor, dir viewer, etc.
+	Window(int pos_x, int pos_y, int size_w, int size_h, void (*task_ptr)(Window&,int)){      // constructor
+		x = pos_x;                                                             // pos and size
+		y = pos_y;
+		w = size_w;
+		h = size_h;
+		task = task_ptr;                                                       // point task to task function
+	}
+	void handle(int key){
+		for(int draw_y = y; draw_y < y + (h + 1); draw_y += h){                // iterate from top (y) to bottom (y + h)
+			for(int draw_x = x; draw_x < x + w; draw_x++){                     // iterate to draw a line from (x) to (x + w)
+				canvas.draw(draw_x,draw_y,'-');                                // draw at x, y
 			}
-			for(int draw_x = x; draw_x < x + (w + 1); draw_x += w){            // iterate from left (x) to right (x + w)
-				for(int draw_y = y; draw_y < y + h; draw_y++){                 // iterate to draw a line from (y) to (y + h)
-					canvas.draw(draw_x,draw_y,'|');                            // draw at x, y
-				}
-			}
-			canvas.draw(x,y,'+');
-			canvas.draw(x + w,y,'+');
-			canvas.draw(x,y + h,'+');
-			canvas.draw(x + w,y + h,'+');
-			task(key);
 		}
+		for(int draw_x = x; draw_x < x + (w + 1); draw_x += w){                // iterate from left (x) to right (x + w)
+			for(int draw_y = y; draw_y < y + h; draw_y++){                     // iterate to draw a line from (y) to (y + h)
+				canvas.draw(draw_x,draw_y,'|');                                // draw at x, y
+			}
+		}
+		canvas.draw(x,y,'+');                                                  // corners
+		canvas.draw(x + w,y,'+');
+		canvas.draw(x,y + h,'+');
+		canvas.draw(x + w,y + h,'+');
+		task(*this,key);                                                       // execute the task
+	}
 };
 
 ///////////////////
@@ -94,13 +115,19 @@ class window{                                                                  /
 
 // testing tasks
 
-void test_task(int key){
+void testTask(Window &parent,int key){                                         // example task parent is for modifing the window ie: editing window.content when that is implemented
+	
+	// KEY ACTIONS
+
 	if(key == 'd'){
 		canvas.draw(5,5,'H');
 		canvas.draw(6,5,'i');
 	}
-	if(key == '>'){
-		canvas.draw(10,10,'>');
+	if(key == 'x'){
+		parent.x += 1;
+	}
+	if(key == 'X'){
+		parent.x -= 1;
 	}
 }
 
@@ -108,4 +135,4 @@ void test_task(int key){
 // WINDOW TYPES
 ///////////////////
 
-window test(0,0,10,10,&test_task);                                // make the window
+Window test(0,0,10,10,&testTask);                                              // make the window
